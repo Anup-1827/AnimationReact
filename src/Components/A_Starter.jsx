6b34ddc02c4bae3288697081334c03ecd0e9c7d5
Anup-1827 <anupalone.off@gmail.com>
2024-06-05 16:53:04 +0530

    Commit

--- src/Components/A_Starter.jsx
+++ src/Components/A_Starter.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 import { animate, motion, useMotionValue, useTransform } from "framer-motion";
 
 function A_Starter() {
 
+    const listRef = useRef();
+
     const x = useMotionValue()
     const opacity = useTransform(x,[-100, 0, 100], [0, 1, 0])
     const list = { hidden: {opacity: 0}}
-    const item = { hidden: { x: -10, opacity:0}}
-    console.log(opacity);
+
+    const variants = {
+      visible: { opacity: 1 },
+      hidden: { opacity: 0 },
+    }
+
+    const item = {
+      visible: { opacity: 1, x: 0 },
+      hidden: { opacity: 0, x: -100 },
+    }
+
+
+
   return (
     <>
       <h2>Introduction</h2>
@@ -31,13 +44,12 @@ function A_Starter() {
         // }}
       ></motion.div>
 
-      <h2>Variants</h2>
+      <h2 >Variants</h2>
 
-      <motion.ul animate="hidden" variants={list}>
-        <motion.li variants={item}/>
-        <motion.li variants={item}/>
-        <motion.li variants={item}/>
-        <motion.li variants={item}/>
+      <motion.ul initial="hidden" animate="visible" variants={variants} transition={{delay: 0.5, duration: 1}}>
+        <li>Hello</li>
+        <li>Hiii</li>
+        <li>Bye</li>
       </motion.ul>
 
       <h2>Motion Value</h2>
